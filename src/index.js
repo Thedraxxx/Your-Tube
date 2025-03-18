46259170a61e28176d05449f239e26edcacdeebc
Roshan Gurung <roshc10grg@gmail.com>
2025-03-18 21:08:04 +0545

    server chalyo

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 import dotenv from "dotenv"
 import connectDatabase from "./db/index.js";
+import app from "./app.js";
 
-dotenv.config({
+dotenv.config({  //load the .env file to access the variable
     path: './env'
 })
-connectDatabase();
+connectDatabase()   //so it return promesies and the it make sure to run the server in given port..
+.then(()=>{
+
+     app.listen(process.env.PORT||8000, ()=>{
+        console.log(`server is running in the port ${process.env.PORT}`)
+     })
+})  
+.catch((Error)=>{
+    console.log("Error while running server..",Error);
+    process.exit(1);
+});
