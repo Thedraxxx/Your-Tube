fb5e45a78ae31b1845e43ddc7195ff0a356ae656
Roshan Gurung <roshc10grg@gmail.com>
2025-03-30 16:23:49 +0545

    add subscription model

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,29 +1,32 @@
 import { Router } from "express";
 import { userRegister } from "../controllers/user.controller.js";
-import  upload  from "../middlwares/multer.middleware.js";
+import upload from "../middlwares/multer.middleware.js";
 import jwtvarify from "../middlwares/auth.middleware.js";
 
-import { userLogin , userLogout , refreshingAccessToken } from "../controllers/user.controller.js";
+import {
+  userLogin,
+  userLogout,
+  refreshingAccessToken,
+} from "../controllers/user.controller.js";
 
 const userRouter = Router();
 
-userRouter.route("/register").post(  // middleware befor the user register for file handling
-    upload.fields([
-        {
-            name: "avatar",
-            maxCount: 1
-        },
-        {
-              name: "coverImage",
-              maxCount: 1
-        }
-    ]),
-    userRegister)
-    userRouter.route("/login").post(upload.none(),userLogin)
-    userRouter.route("/logout").post(jwtvarify,
-    userLogout
-)
+userRouter.route("/register").post(
+  // middleware befor the user register for file handling
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  userRegister
+);
+userRouter.route("/login").post(upload.none(), userLogin);
+userRouter.route("/logout").post(jwtvarify, userLogout);
 userRouter.route("/refres-token").post(refreshingAccessToken);
-
 
 export default userRouter;
