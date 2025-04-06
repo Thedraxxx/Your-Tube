768bf720c64baa8f4debbe44f72f5671f51ebaf7
Roshan Gurung <roshc10grg@gmail.com>
2025-04-06 18:33:05 +0545

    feat: implement edit comment functionality; add editComment controller and route for updating comments

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -2,14 +2,16 @@ import ApiError from "../utils/APIError.js";
 import { asyncHandler } from "../utils/asyncHandeler.js";
 import Comment from "../models/comments.model.js";
 import ApiResponse from "../utils/APIrsponse.js";
-
+/*
+  publish Comment..
+*/
 const publishComment = asyncHandler(async(req,res)=>{
          const { content } =  req.body;
          const { videoId } = req.params;
          const userId = req.user._id;
-         console.log(req.body);
-         console.log(videoId);
-         console.log(userId);
+        //  console.log(req.body);
+        //  console.log(videoId);
+        //  console.log(userId);
 
         if(!content){
             throw new ApiError(400,"enpty content");
@@ -23,5 +25,26 @@ const publishComment = asyncHandler(async(req,res)=>{
             new ApiResponse(200,commentContant,"commented successfully")
         )
 });
-
-export {publishComment}
+/*
+   edit comment
+*/
+const editComment = asyncHandler(async(req,res)=> {
+   const {content} = req.body;
+    const {commentId} =  req.params;
+     if(!content.trim()){
+        throw new ApiError(400,"enpty content");
+     }
+   const editedComment = await Comment.findByIdAndUpdate(commentId,
+        {
+            $set: {content}
+        },{
+            new: true,
+        }
+    )
+    return res.status(200).json(
+        new ApiResponse(200,editedComment,"comment edited successfully")
+    )
+});
+const deleteComment = asyncHandler(async(req,res)=> {});
+const editCommentt = asyncHandler(async(req,res)=> {});
+export {publishComment, editComment, deleteComment }
--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import jwtvarify from "../middlwares/auth.middleware.js";
-import { publishComment } from "../controllers/comment.controller.js";
+import { publishComment, editComment, } from "../controllers/comment.controller.js";
 
 const commentRouter = Router();
 
 commentRouter.route("/:videoId").post(jwtvarify,publishComment);
+commentRouter.route("/edit/:commentId").patch(jwtvarify,editComment);
+// commentRouter.route("/:commentId").delete(jwtvarify,deleteComment);
 export default commentRouter;
