b30af57aced75ee26957e6fc3edc41f87251ef55
Roshan Gurung <roshc10grg@gmail.com>
2025-04-07 23:50:37 +0545

    feat: implement toggleCommentLike functionality; add controller and route for managing comment likes

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -12,22 +12,47 @@ const toggleVideoLike = asyncHandler(async(req,res)=>{
          }
         const existingLike = await Like.findById({
             videos: videoId,
-            likedBy: userId,
+            likedby: userId,
         }); 
         if(existingLike){
-              await Like.deleteOne();
+              await existingLike.deleteOne();
               return res.status(200).json(new ApiResponse(200,null,"removed like successfully."));
         }else{
            const newLike = await Like.create({
                 videos: videoId,
-                likeBy: userId,
+                likedby: userId,
         });
         return res.status(200).json(new ApiResponse(200,newLike,"liked successfully."));
         
         }
-         
+});
+const toggleCommentLike = asyncHandler(async(req,res)=>{
+       const {commentId} = req.params;
+       const userId = req.user._id;
 
+       if(!commentId){
+            throw new ApiError(404,"comment is not provided")
+       }
+       const existingLike = await Like.findById({
+            comment: commentId,
+            likedby: userId
+        })
+        if(existingLike){
+            await existingLike.deleteOne();
+        return res.status(200).json( new ApiResponse(200, null , " unliked the comment"))
+        }
+        else{
+           const newLike = await Like.create({
+                comment: commentId,
+                likedby: userId
+            });
+        return res.status(200).json( new ApiResponse(200, newLike , " liked the comment"))
+            
+        }
+});
+
+const getLikedVideos = asyncHandler(async(req,res)=>{
 
 })
 
-export {toggleVideoLike}
+export {toggleVideoLike, toggleCommentLike}
--- src/routes/like.route.js
+++ src/routes/like.route.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import jwtvarify from "../middlwares/auth.middleware";
-
+import { toggleCommentLike, toggleVideoLike } from "../controllers/like.controller.js";
 const likeRouter = Router();
 
 likeRouter.route("/toggle/:videoId").post(jwtvarify,toggleVideoLike);
+likeRouter.route("/toggle/:commentId").post(jwtvarify,toggleCommentLike);
 export default likeRouter;
